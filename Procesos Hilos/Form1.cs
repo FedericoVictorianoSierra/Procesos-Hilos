326be51ffa3ffece70ef9d83447e5257ceb6d1ce
FedericoVS <71857813+FedericoVictorianoSierra@users.noreply.github.com>
2022-09-13 11:45:12 -0500

    Cambios

--- Procesos Hilos/Form1.cs
+++ Procesos Hilos/Form1.cs
@@ -22,21 +22,49 @@ namespace Procesos_Hilos
         {
             Hilo();
         }
-
+        Thread miHilo;
         void Hilo()
         {
-            new Thread(
+            Thread miHilo = new Thread(simulacionHilo);
+            miHilo.Start();
+            Thread.Sleep(1000);
+            // Abort newThread.
+            /*Console.WriteLine("Main aborting new thread.");
+            miHilo.Abort();
+           
+            // Wait for the thread to terminate.
+            miHilo.Join();
+            Console.WriteLine("New thread terminated - Main exiting.");*/
+            /*new Thread(
                 () =>
                 {
                     while (true)
                     {
                         
                         procesoImg.Location = new Point(procesoImg.Location.X + 1, procesoImg.Location.Y);
-                        Thread.Sleep(1);
+                        //Thread.Sleep();
+                        Console.WriteLine("EWjecutandose Hilo");
                     }
                 }
                 )
-            { IsBackground = true }.Start();
+            { IsBackground = true }.Start();*/
+        }
+
+        void simulacionHilo()
+        {
+            try
+            {
+                while (true)
+                {
+                    procesoImg.Location = new Point(procesoImg.Location.X + 1, procesoImg.Location.Y);
+                    Console.WriteLine("Ejecutandose Hilo");
+                    Thread.Sleep(1);
+                }
+            }
+            catch (ThreadAbortException abortException)
+            {
+                Console.WriteLine((string)abortException.ExceptionState);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
