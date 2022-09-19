d9ba0887abe6e13c7bc1ef8e886327b60c2a848d
José Antonio Balbuena <JoseABalbuena2@hotmail.com>
2022-09-19 15:57:59 -0500

    tamaño del imagen en ROUNROBIN

--- Procesos Hilos/ROUNDROBIN.Designer.cs
+++ Procesos Hilos/ROUNDROBIN.Designer.cs
@@ -44,9 +44,10 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(96)))), ((int)(((byte)(97)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(26, 29);
+            this.button1.Location = new System.Drawing.Point(35, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 38);
+            this.button1.Size = new System.Drawing.Size(140, 47);
             this.button1.TabIndex = 1;
             this.button1.Text = "Tarea";
             this.button1.UseVisualStyleBackColor = false;
@@ -56,9 +57,10 @@
             // 
             this.procesoImg.BackColor = System.Drawing.Color.Transparent;
             this.procesoImg.Image = global::Procesos_Hilos.Properties.Resources.proceso;
-            this.procesoImg.Location = new System.Drawing.Point(75, 105);
+            this.procesoImg.Location = new System.Drawing.Point(100, 129);
+            this.procesoImg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.procesoImg.Name = "procesoImg";
-            this.procesoImg.Size = new System.Drawing.Size(76, 78);
+            this.procesoImg.Size = new System.Drawing.Size(101, 96);
             this.procesoImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.procesoImg.TabIndex = 3;
             this.procesoImg.TabStop = false;
@@ -67,9 +69,10 @@
             // 
             this.procesadorImg.BackColor = System.Drawing.Color.Transparent;
             this.procesadorImg.Image = global::Procesos_Hilos.Properties.Resources.procesador;
-            this.procesadorImg.Location = new System.Drawing.Point(415, 129);
+            this.procesadorImg.Location = new System.Drawing.Point(476, 93);
+            this.procesadorImg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.procesadorImg.Name = "procesadorImg";
-            this.procesadorImg.Size = new System.Drawing.Size(309, 182);
+            this.procesadorImg.Size = new System.Drawing.Size(645, 448);
             this.procesadorImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.procesadorImg.TabIndex = 4;
             this.procesadorImg.TabStop = false;
@@ -78,9 +81,10 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::Procesos_Hilos.Properties.Resources.proceso;
-            this.pictureBox1.Location = new System.Drawing.Point(75, 216);
+            this.pictureBox1.Location = new System.Drawing.Point(100, 266);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(76, 78);
+            this.pictureBox1.Size = new System.Drawing.Size(101, 96);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -89,23 +93,25 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::Procesos_Hilos.Properties.Resources.proceso;
-            this.pictureBox2.Location = new System.Drawing.Point(75, 329);
+            this.pictureBox2.Location = new System.Drawing.Point(100, 405);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(76, 78);
+            this.pictureBox2.Size = new System.Drawing.Size(101, 96);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.procesadorImg);
             this.Controls.Add(this.procesoImg);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form3";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form3_Load);
