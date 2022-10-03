7397147dac6b585114bf36492d682ba15e9a57ff
FedericoVS <minishxco@gmail.com>
2022-10-03 00:53:34 -0500

    Pagination (prueba)

---
+++ Procesos Hilos/PAGINATION.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace Procesos_Hilos
+{
+    partial class PAGINATION
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.slot0 = new System.Windows.Forms.Button();
+            this.slot1 = new System.Windows.Forms.Button();
+            this.slot2 = new System.Windows.Forms.Button();
+            this.slot3 = new System.Windows.Forms.Button();
+            this.slot4 = new System.Windows.Forms.Button();
+            this.slot5 = new System.Windows.Forms.Button();
+            this.slot6 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // slot0
+            // 
+            this.slot0.BackColor = System.Drawing.Color.Transparent;
+            this.slot0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slot0.Location = new System.Drawing.Point(483, 74);
+            this.slot0.Name = "slot0";
+            this.slot0.Size = new System.Drawing.Size(125, 35);
+            this.slot0.TabIndex = 1;
+            this.slot0.UseVisualStyleBackColor = false;
+            // 
+            // slot1
+            // 
+            this.slot1.BackColor = System.Drawing.Color.Transparent;
+            this.slot1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slot1.Location = new System.Drawing.Point(483, 115);
+            this.slot1.Name = "slot1";
+            this.slot1.Size = new System.Drawing.Size(125, 35);
+            this.slot1.TabIndex = 2;
+            this.slot1.UseVisualStyleBackColor = false;
+            // 
+            // slot2
+            // 
+            this.slot2.BackColor = System.Drawing.Color.Transparent;
+            this.slot2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slot2.Location = new System.Drawing.Point(483, 156);
+            this.slot2.Name = "slot2";
+            this.slot2.Size = new System.Drawing.Size(125, 35);
+            this.slot2.TabIndex = 3;
+            this.slot2.UseVisualStyleBackColor = false;
+            // 
+            // slot3
+            // 
+            this.slot3.BackColor = System.Drawing.Color.Transparent;
+            this.slot3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slot3.Location = new System.Drawing.Point(483, 197);
+            this.slot3.Name = "slot3";
+            this.slot3.Size = new System.Drawing.Size(125, 35);
+            this.slot3.TabIndex = 4;
+            this.slot3.UseVisualStyleBackColor = false;
+            // 
+            // slot4
+            // 
+            this.slot4.BackColor = System.Drawing.Color.Transparent;
+            this.slot4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slot4.Location = new System.Drawing.Point(483, 238);
+            this.slot4.Name = "slot4";
+            this.slot4.Size = new System.Drawing.Size(125, 35);
+            this.slot4.TabIndex = 5;
+            this.slot4.UseVisualStyleBackColor = false;
+            // 
+            // slot5
+            // 
+            this.slot5.BackColor = System.Drawing.Color.Transparent;
+            this.slot5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slot5.Location = new System.Drawing.Point(483, 279);
+            this.slot5.Name = "slot5";
+            this.slot5.Size = new System.Drawing.Size(125, 35);
+            this.slot5.TabIndex = 6;
+            this.slot5.UseVisualStyleBackColor = false;
+            // 
+            // slot6
+            // 
+            this.slot6.BackColor = System.Drawing.Color.Transparent;
+            this.slot6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.slot6.Location = new System.Drawing.Point(483, 320);
+            this.slot6.Name = "slot6";
+            this.slot6.Size = new System.Drawing.Size(125, 35);
+            this.slot6.TabIndex = 7;
+            this.slot6.UseVisualStyleBackColor = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(177, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(79, 329);
+            this.listBox1.TabIndex = 15;
+            // 
+            // PAGINATION
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.slot6);
+            this.Controls.Add(this.slot5);
+            this.Controls.Add(this.slot4);
+            this.Controls.Add(this.slot3);
+            this.Controls.Add(this.slot2);
+            this.Controls.Add(this.slot1);
+            this.Controls.Add(this.slot0);
+            this.Controls.Add(this.button1);
+            this.Name = "PAGINATION";
+            this.Text = "Form4";
+            this.Load += new System.EventHandler(this.PAGINATION_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button slot0;
+        private System.Windows.Forms.Button slot1;
+        private System.Windows.Forms.Button slot2;
+        private System.Windows.Forms.Button slot3;
+        private System.Windows.Forms.Button slot4;
+        private System.Windows.Forms.Button slot5;
+        private System.Windows.Forms.Button slot6;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ Procesos Hilos/PAGINATION.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Procesos_Hilos
+{
+    public partial class PAGINATION : Form
+    {
+        public PAGINATION()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private Button[] Marco = new Button[7];//tamaño de pagina
+        private void Start()
+        {
+            Marco[0] = slot0;
+            Marco[1] = slot1;
+            Marco[2] = slot2;
+            Marco[3] = slot3;
+            Marco[4] = slot4;
+            Marco[5] = slot5;
+            Marco[6] = slot6;
+        }
+        Random rand = new Random();
+        private List<Thread> miHilo = new List<Thread>();
+        private List<int> TamanioPagina = new List<int>();
+        private List<Color> colorSlot = new List<Color>();
+        private Queue<int> iteration = new Queue<int>();
+        private void Simular()
+        {
+            TamanioPagina.Add(rand.Next(1, 4));
+            colorSlot.Add(Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255)));
+            miHilo.Add(new Thread(simulacionHilo));
+            listBox1.Items.Add(TamanioPagina.ElementAt(TamanioPagina.Count-1));
+            StartThread();
+        }
+
+        public void StartThread()
+        {
+            int vacios = 0;
+            for (int k = 0; k < Marco.Length; k++)
+            {
+                if (Marco[k].BackColor == Color.Transparent)
+                {
+                    vacios++;//contar cuantos marcos estan vacios
+                }
+            }
+            for (int i = 0; i < TamanioPagina.Count; i++)// buscar entre todos los espacios de cada proceso
+            {
+                int num = TamanioPagina.ElementAt(i);
+                if (TamanioPagina.ElementAt(i) <= vacios)//el que sea menor o igual a los desocupados
+                {
+                    if (miHilo.ElementAt(i).ThreadState == ThreadState.Unstarted)
+                    {
+                        for (int j = 0; j < Marco.Length; j++)//ocupar segun el tamaño de las paginas que contenga el proceso
+                        {
+                            if (Marco[j].BackColor == Color.Transparent && num > 0)//verificar que el espacio este vacio
+                            {
+                                Marco[j].BackColor = colorSlot.ElementAt(i);//ocupar el espacio indicando con color
+                                num--;
+                            }
+                        }
+
+                        //iniciar proceso
+                        listBox1.Items.RemoveAt(0);
+                        iteration.Enqueue(i);
+                        miHilo.ElementAt(i).Start();
+                    }
+                }
+                else
+                {
+                    //se va a espera
+                    //break;
+                }
+            }
+        }
+
+        public void simulacionHilo()
+        {
+            try
+            {
+                while (true)
+                {
+                    Thread.Sleep(rand.Next(4000, 8000));
+                    miHilo.ElementAt(0).Abort();
+                }
+            }
+            catch (ThreadAbortException abortException)
+            {
+                //Console.WriteLine((string)abortException.ExceptionState);
+            }
+            finally
+            {
+                for (int i = 0; i < Marco.Length; i++)
+                {
+                    if (Marco[i].BackColor == colorSlot.ElementAt(0))
+                    {
+                        Marco[i].BackColor = Color.Transparent;
+                    }
+                }
+
+                colorSlot.RemoveAt(0);
+                TamanioPagina.RemoveAt(0);
+                miHilo.RemoveAt(0);
+                iteration.Dequeue();
+                if (miHilo.Count > 0)
+                {
+                    //dar paso al siguiente
+                    StartThread();
+                }
+            }
+        }
+
+        private void PAGINATION_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Simular();
+        }
+    }
+}
