cab49fc0d9fb0b66eaf5461c76367e514fd768c9
FedericoVS <minishxco@gmail.com>
2022-10-09 01:07:00 -0500

    SEGMENTATION

---
+++ Procesos Hilos/SEGMENTATION.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace Procesos_Hilos
+{
+    partial class SEGMENTATION
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.segmento5 = new System.Windows.Forms.Button();
+            this.segmento4 = new System.Windows.Forms.Button();
+            this.segmento3 = new System.Windows.Forms.Button();
+            this.segmento2 = new System.Windows.Forms.Button();
+            this.segmento0 = new System.Windows.Forms.Button();
+            this.i1 = new System.Windows.Forms.PictureBox();
+            this.i4 = new System.Windows.Forms.PictureBox();
+            this.i0 = new System.Windows.Forms.PictureBox();
+            this.i3 = new System.Windows.Forms.PictureBox();
+            this.i2 = new System.Windows.Forms.PictureBox();
+            this.segmento1 = new System.Windows.Forms.Button();
+            this.i5 = new System.Windows.Forms.PictureBox();
+            this.i6 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.imgProcesador = new System.Windows.Forms.PictureBox();
+            this.ram = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.i1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgProcesador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ram)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // segmento5
+            // 
+            this.segmento5.BackColor = System.Drawing.Color.Transparent;
+            this.segmento5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.segmento5.Location = new System.Drawing.Point(657, 351);
+            this.segmento5.Name = "segmento5";
+            this.segmento5.Size = new System.Drawing.Size(90, 75);
+            this.segmento5.TabIndex = 14;
+            this.segmento5.UseVisualStyleBackColor = false;
+            // 
+            // segmento4
+            // 
+            this.segmento4.BackColor = System.Drawing.Color.Transparent;
+            this.segmento4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.segmento4.Location = new System.Drawing.Point(657, 295);
+            this.segmento4.Name = "segmento4";
+            this.segmento4.Size = new System.Drawing.Size(90, 50);
+            this.segmento4.TabIndex = 11;
+            this.segmento4.UseVisualStyleBackColor = false;
+            // 
+            // segmento3
+            // 
+            this.segmento3.BackColor = System.Drawing.Color.Transparent;
+            this.segmento3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.segmento3.Location = new System.Drawing.Point(657, 264);
+            this.segmento3.Name = "segmento3";
+            this.segmento3.Size = new System.Drawing.Size(90, 25);
+            this.segmento3.TabIndex = 10;
+            this.segmento3.UseVisualStyleBackColor = false;
+            // 
+            // segmento2
+            // 
+            this.segmento2.BackColor = System.Drawing.Color.Transparent;
+            this.segmento2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.segmento2.Location = new System.Drawing.Point(657, 208);
+            this.segmento2.Name = "segmento2";
+            this.segmento2.Size = new System.Drawing.Size(90, 50);
+            this.segmento2.TabIndex = 9;
+            this.segmento2.UseVisualStyleBackColor = false;
+            // 
+            // segmento0
+            // 
+            this.segmento0.BackColor = System.Drawing.Color.Transparent;
+            this.segmento0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.segmento0.Location = new System.Drawing.Point(657, 21);
+            this.segmento0.Name = "segmento0";
+            this.segmento0.Size = new System.Drawing.Size(90, 100);
+            this.segmento0.TabIndex = 15;
+            this.segmento0.UseVisualStyleBackColor = false;
+            // 
+            // i1
+            // 
+            this.i1.BackColor = System.Drawing.Color.Peru;
+            this.i1.Image = global::Procesos_Hilos.Properties.Resources.proceso;
+            this.i1.Location = new System.Drawing.Point(139, 71);
+            this.i1.Name = "i1";
+            this.i1.Size = new System.Drawing.Size(90, 25);
+            this.i1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i1.TabIndex = 30;
+            this.i1.TabStop = false;
+            // 
+            // i4
+            // 
+            this.i4.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.i4.Image = global::Procesos_Hilos.Properties.Resources.proceso;
+            this.i4.Location = new System.Drawing.Point(139, 239);
+            this.i4.Name = "i4";
+            this.i4.Size = new System.Drawing.Size(90, 50);
+            this.i4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i4.TabIndex = 29;
+            this.i4.TabStop = false;
+            // 
+            // i0
+            // 
+            this.i0.BackColor = System.Drawing.Color.Orchid;
+            this.i0.Image = global::Procesos_Hilos.Properties.Resources.proceso;
+            this.i0.Location = new System.Drawing.Point(139, 15);
+            this.i0.Name = "i0";
+            this.i0.Size = new System.Drawing.Size(90, 50);
+            this.i0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i0.TabIndex = 28;
+            this.i0.TabStop = false;
+            // 
+            // i3
+            // 
+            this.i3.BackColor = System.Drawing.Color.Gold;
+            this.i3.Image = global::Procesos_Hilos.Properties.Resources.proceso;
+            this.i3.Location = new System.Drawing.Point(139, 183);
+            this.i3.Name = "i3";
+            this.i3.Size = new System.Drawing.Size(90, 50);
+            this.i3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i3.TabIndex = 27;
+            this.i3.TabStop = false;
+            // 
+            // i2
+            // 
+            this.i2.BackColor = System.Drawing.Color.PaleGreen;
+            this.i2.Image = global::Procesos_Hilos.Properties.Resources.proceso;
+            this.i2.Location = new System.Drawing.Point(139, 102);
+            this.i2.Name = "i2";
+            this.i2.Size = new System.Drawing.Size(90, 75);
+            this.i2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i2.TabIndex = 26;
+            this.i2.TabStop = false;
+            // 
+            // segmento1
+            // 
+            this.segmento1.BackColor = System.Drawing.Color.Transparent;
+            this.segmento1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.segmento1.Location = new System.Drawing.Point(657, 127);
+            this.segmento1.Name = "segmento1";
+            this.segmento1.Size = new System.Drawing.Size(90, 75);
+            this.segmento1.TabIndex = 31;
+            this.segmento1.UseVisualStyleBackColor = false;
+            // 
+            // i5
+            // 
+            this.i5.BackColor = System.Drawing.Color.SlateBlue;
+            this.i5.Image = global::Procesos_Hilos.Properties.Resources.proceso;
+            this.i5.Location = new System.Drawing.Point(139, 295);
+            this.i5.Name = "i5";
+            this.i5.Size = new System.Drawing.Size(90, 25);
+            this.i5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i5.TabIndex = 32;
+            this.i5.TabStop = false;
+            // 
+            // i6
+            // 
+            this.i6.BackColor = System.Drawing.Color.Pink;
+            this.i6.Image = global::Procesos_Hilos.Properties.Resources.proceso;
+            this.i6.Location = new System.Drawing.Point(139, 326);
+            this.i6.Name = "i6";
+            this.i6.Size = new System.Drawing.Size(90, 100);
+            this.i6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.i6.TabIndex = 33;
+            this.i6.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 40);
+            this.button3.TabIndex = 34;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // imgProcesador
+            // 
+            this.imgProcesador.Image = global::Procesos_Hilos.Properties.Resources.procesador;
+            this.imgProcesador.Location = new System.Drawing.Point(289, 71);
+            this.imgProcesador.Name = "imgProcesador";
+            this.imgProcesador.Size = new System.Drawing.Size(261, 309);
+            this.imgProcesador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgProcesador.TabIndex = 35;
+            this.imgProcesador.TabStop = false;
+            // 
+            // ram
+            // 
+            this.ram.Image = global::Procesos_Hilos.Properties.Resources.RAM;
+            this.ram.Location = new System.Drawing.Point(596, 22);
+            this.ram.Name = "ram";
+            this.ram.Size = new System.Drawing.Size(190, 416);
+            this.ram.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ram.TabIndex = 36;
+            this.ram.TabStop = false;
+            // 
+            // SEGMENTATION
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 566);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.i6);
+            this.Controls.Add(this.i5);
+            this.Controls.Add(this.segmento1);
+            this.Controls.Add(this.i1);
+            this.Controls.Add(this.i4);
+            this.Controls.Add(this.i0);
+            this.Controls.Add(this.i3);
+            this.Controls.Add(this.i2);
+            this.Controls.Add(this.segmento0);
+            this.Controls.Add(this.segmento5);
+            this.Controls.Add(this.segmento4);
+            this.Controls.Add(this.segmento3);
+            this.Controls.Add(this.segmento2);
+            this.Controls.Add(this.imgProcesador);
+            this.Controls.Add(this.ram);
+            this.Name = "SEGMENTATION";
+            this.Text = "SEGMENTATION";
+            this.Load += new System.EventHandler(this.SEGMENTATION_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.i1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.i6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgProcesador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ram)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button segmento5;
+        private System.Windows.Forms.Button segmento4;
+        private System.Windows.Forms.Button segmento3;
+        private System.Windows.Forms.Button segmento2;
+        private System.Windows.Forms.Button segmento0;
+        private System.Windows.Forms.PictureBox i1;
+        private System.Windows.Forms.PictureBox i4;
+        private System.Windows.Forms.PictureBox i0;
+        private System.Windows.Forms.PictureBox i3;
+        private System.Windows.Forms.PictureBox i2;
+        private System.Windows.Forms.Button segmento1;
+        private System.Windows.Forms.PictureBox i5;
+        private System.Windows.Forms.PictureBox i6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox imgProcesador;
+        private System.Windows.Forms.PictureBox ram;
+    }
+}
---
+++ Procesos Hilos/SEGMENTATION.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Procesos_Hilos
+{
+    public partial class SEGMENTATION : Form
+    {
+        public SEGMENTATION()
+        {
+            InitializeComponent();
+        }
+
+        private Button[] Marco = new Button[6];//tamaño de segmento
+        private PictureBox[] proceso = new PictureBox[7];
+        private Thread[] miHilo = new Thread[7];
+        Random rand = new Random();
+        private void Start()
+        {
+            Marco[0] = segmento0;
+            Marco[1] = segmento1;
+            Marco[2] = segmento2;
+            Marco[3] = segmento3;
+            Marco[4] = segmento4;
+            Marco[5] = segmento5;
+
+            proceso[0] = i0;
+            proceso[1] = i1;
+            proceso[2] = i2;
+            proceso[3] = i3;
+            proceso[4] = i4;
+            proceso[5] = i5;
+            proceso[6] = i6;
+
+            miHilo[0] = new Thread(simulacionHilo0);
+            miHilo[1] = new Thread(simulacionHilo1);
+            miHilo[2] = new Thread(simulacionHilo2);
+            miHilo[3] = new Thread(simulacionHilo3);
+            miHilo[4] = new Thread(simulacionHilo4);
+            miHilo[5] = new Thread(simulacionHilo5);
+            miHilo[6] = new Thread(simulacionHilo6);
+        }
+        private void Simular()
+        {
+            Start();
+            for (int i = 0; i < proceso.Length; i++)
+            {
+                proceso[i].Visible = true;
+                proceso[i].Location = new Point(139, proceso[i].Location.Y);
+            }
+
+            for (int i = 0; i < miHilo.Length; i++)
+            {
+                miHilo[i].Start();//iniciamos todos los hilos
+            }
+        }
+
+        void Proceso(PictureBox image, int hilo)
+        {
+            bool Active = false;
+            int j = 0;
+            try
+            {
+                while (true)
+                {
+                    Thread.Sleep(20);
+                    if (image.Location.X >= imgProcesador.Location.X)
+                    {
+                        for (int i = 0; i < Marco.Length; i++)
+                        {
+                            if(image.Size.Height == Marco[i].Size.Height)
+                            {
+                                if (Marco[i].BackColor == Color.Transparent && !Active)
+                                {
+                                    Marco[i].BackColor = image.BackColor;
+                                    j = i;
+                                    Active = true;
+                                }
+                            }
+                        }
+                        if(Active)
+                        {
+                            if(Marco[j].BackColor != image.BackColor)
+                            {
+                                Active = false;
+                            }
+                            else
+                            {
+                                image.Location = new Point(image.Location.X + 1, image.Location.Y);//avanzar
+                                if (image.Location.X >= ram.Location.X)
+                                {
+                                    image.Visible = false;
+                                    miHilo[hilo].Abort();
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        image.Location = new Point(image.Location.X + 1, image.Location.Y);//avanzar
+                    }
+                }
+            }
+            catch (ThreadAbortException abortException)
+            {
+                Console.WriteLine((string)abortException.ExceptionState);
+            }
+            finally
+            {
+                for (int i = 0; i < Marco.Length; i++)
+                {
+                    if (Marco[i].BackColor == image.BackColor)
+                    {
+                        Marco[i].BackColor = Color.Transparent;
+                    }
+                }
+            }
+        }
+        void simulacionHilo0()
+        {
+            Proceso(i0, 0);
+        }
+        void simulacionHilo1()
+        {
+            Proceso(i1, 1);
+        }
+        void simulacionHilo2()
+        {
+            Proceso(i2, 2);
+        }
+        void simulacionHilo3()
+        {
+            Proceso(i3, 3);
+        }
+        void simulacionHilo4()
+        {
+            Proceso(i4, 4);
+        }
+
+        void simulacionHilo5()
+        {
+            Proceso(i5, 5);
+        }
+
+        void simulacionHilo6()
+        {
+            Proceso(i6, 6);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Simular();
+        }
+
+        private void SEGMENTATION_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
